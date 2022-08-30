2cc292bfff589e31dd1b4c041a417b6aa17373be
Ygor Frazao <ygor_frazao@hotmail.com>
2022-08-30 10:32:28 -0300

    Alteracoes no layout do roll e yaw

--- src/pages/Config/index.tsx
+++ src/pages/Config/index.tsx
@@ -1,14 +1,16 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import {View, Text, TouchableOpacity} from 'react-native';
 import { orientationAngle } from 'react-native-orientation-angle'
 import {styles} from './style';
 import AsyncStorage from '@react-native-community/async-storage';
 
 
+
+
 let exportAxies: any;
 
   const Configurations = () =>{
-
+;
   const [interval, setInterval] = useState(0)
   const [result, setResult] = useState({ pitch: 0, roll: 0, yaw: 0 })
 
@@ -26,7 +28,7 @@ let exportAxies: any;
   const renderValue = (label: string, value: string) => (
     <View style={styles.box}>
       <Text style={styles.renderText}>{label}: </Text>
-      <Text style={styles.renderText}>{value}</Text>
+      <Text style={styles.renderText}>{value} </Text>
     </View>
   )
 
@@ -43,9 +45,20 @@ let exportAxies: any;
 
   async function saveAxies()
   {
+    
     saveData(result);
     
     orientationAngle.unsubscribe()
+
+  }
+  async function saveAxiesDefault()
+  {
+
+    saveData({ pitch: 0, roll: 0, yaw: 0 });
+    setResult({ pitch: 0, roll: 0, yaw: 0 });
+
+    orientationAngle.unsubscribe()
+
 
   }
 
@@ -53,17 +66,17 @@ let exportAxies: any;
     <View style={styles.container}>
         <Text style={styles.titleText}> Configurações de posição inicial </Text>
           <View style={styles.box}>
-            {renderValue('pitch', result.pitch.toFixed(2))}
-            {renderValue('roll', result.roll.toFixed(2))}
-            {renderValue('yaw', result.yaw.toFixed(2))}
+            {renderValue('Pitch', result.pitch.toFixed(2))}
+            {renderValue('Roll', result.roll.toFixed(2))}
+            {renderValue('Yaw', result.yaw.toFixed(2))}
           </View>
+          <TouchableOpacity style={styles.button} onPress={() => {saveAxiesDefault();}}>
+              <Text style={styles.buttonText}>Resetar Posição</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => {saveAxies();}}>
+              <Text style={styles.buttonText}>Salvar Posição Atual</Text>
+          </TouchableOpacity>
 
-          <TouchableOpacity style={styles.button} onPress={() => {saveAxies();}}>
-              <Text style={styles.renderText}>Salvar Posição</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => {saveAxies();}}>
-              <Text style={styles.renderText}>Salvar Origem</Text>
-          </TouchableOpacity>
     </View>
   )
 }
